ed8440850e4787042c119b77631edf9af517f0b1
deviant <mb.deviant@gmail.com>
2023-05-01 19:32:05 +0300

    edit indentation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,322 @@
 // MARK FOR UI ELEMENTS, MAKE THEM SEPERATE MODULE
+
+// BUTTON LOGIC
 const flipButton = document.getElementById("flip-button");
-const shipContainer = document.querySelector(".ship-option-container");
+const shipContainer = document.querySelector(".ship-select-container");
 
 flipButton.addEventListener("click", flipShips);
 
+let angle = 0;
 function flipShips() {
-    const ships = Array.from(shipContainer.children);
-    ships.forEach((ship) => {
-        // eslint-disable-next-line no-param-reassign
-        ship.style.transform = `rotate(90deg)`;
+  const ships = Array.from(shipContainer.children);
+  angle = angle === 0 ? 90 : 0;
+  ships.forEach((ship) => {
+    // eslint-disable-next-line no-param-reassign
+    ship.style.transform = `rotate(${angle}deg)`;
+  });
+}
+
+// CREATING GAMEBOARD
+function createBoard(color, user) {
+  const gameBoardContainer = document.getElementById("gameboard-container");
+
+  const gameBoard = document.createElement("div");
+  gameBoard.setAttribute("id", user);
+  gameBoard.classList.add("gameboard");
+  gameBoard.style.backgroundColor = color;
+
+  for (let i = 0; i < 100; i += 1) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.id = i;
+    gameBoard.append(block);
+  }
+
+  gameBoardContainer.appendChild(gameBoard);
+}
+
+createBoard("white", "player");
+createBoard("gainsboro", "computer");
+
+// CREATING SHIPS
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+  }
+}
+
+const destroyer = new Ship("destroyer", 2);
+const submarine = new Ship("submarine", 3);
+const cruiser = new Ship("cruiser", 3);
+const battleship = new Ship("battleship", 4);
+const carrier = new Ship("carrier", 5);
+
+const ships = [destroyer, submarine, cruiser, battleship, carrier];
+let notDropped;
+console.log(ships);
+
+function checkValidity(boardBlocks, isHorizontal, startIndex, ship) {
+  // to prevent placing ships off board
+  // eslint-disable-next-line no-nested-ternary
+  const validStart = isHorizontal
+    ? startIndex <= 10 * 10 - ship.length
+      ? startIndex
+      : 10 * 10 - ship.length
+    : startIndex <= 10 * 10 - 10 * ship.length
+    ? startIndex
+    : startIndex - ship.length * 10 + 10;
+  console.log(`validated i ${validStart}`);
+
+  const shipBlocks = [];
+  // save the indexes of ships to an array
+  for (let i = 0; i < ship.length; i += 1) {
+    if (isHorizontal) shipBlocks.push(boardBlocks[Number(validStart) + i]);
+    else shipBlocks.push(boardBlocks[Number(validStart) + i * 10]);
+  }
+
+  // validate place to prevent ships from splitting
+  let isValid;
+  if (isHorizontal) {
+    shipBlocks.every(
+      // eslint-disable-next-line no-return-assign
+      (_block, index) =>
+        (isValid =
+          shipBlocks[0].id % 10 !== 10 - (shipBlocks.length - (index + 1)))
+    );
+  } else {
+    shipBlocks.every(
+      // eslint-disable-next-line no-return-assign
+      (_block, index) => (isValid = shipBlocks[0].id < 90 + (10 * index + 1))
+    );
+  }
+  // console.log(`is valid? ${isValid}`);
+  const notTaken = shipBlocks.every(
+    (block) => !block.classList.contains("filled")
+  );
+
+  return { shipBlocks, isValid, notTaken };
+}
+
+function addShip(user, ship, startId) {
+  console.log(user);
+  const boardBlocks = document.querySelectorAll(`#${user} div`);
+  const bool = Math.random() < 0.5; // returned number either will be bigger than 0.5 or not hence the random bool
+  const isHorizontal = user === "player" ? angle === 0 : bool;
+  const randomStartIndex = Math.floor(Math.random() * 10 * 10); // ten times ten is the width of the board
+  console.log(`horizontal ${isHorizontal}`);
+  console.log(`random index ${randomStartIndex}`);
+
+  const startIndex = startId || randomStartIndex;
+  console.log(`start index ${startIndex}`);
+
+  const { shipBlocks, isValid, notTaken } = checkValidity(
+    boardBlocks,
+    isHorizontal,
+    startIndex,
+    ship
+  );
+
+  // console.log(`not taken? ${notTaken}`);
+  if (isValid && notTaken) {
+    shipBlocks.forEach((block) => {
+      block.classList.add(ship.name);
+      block.classList.add("filled");
     });
+  } else {
+    if (user === "computer") addShip(user, ship, startId);
+    if (user === "player") notDropped = true;
+  }
+}
+// addShip(destroyer);
+// addShip(submarine);
+// addShip(cruiser);
+// addShip(battleship);
+// addShip("computer", carrier);
+ships.forEach((ship) => addShip("computer", ship));
+
+// DRAG&DROP PLAYER SHIPS
+let draggedShip;
+const shipOptions = Array.from(shipContainer.children);
+shipOptions.forEach((ship) => ship.addEventListener("dragstart", dragStart));
+
+const playerBoard = document.querySelectorAll("#player div");
+playerBoard.forEach((block) => {
+  block.addEventListener("dragover", dragOver);
+  block.addEventListener("drop", dropShip);
+});
+
+function dragStart(e) {
+  notDropped = false;
+  draggedShip = e.target;
+  console.log(draggedShip);
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  const ship = ships[draggedShip.id];
+  console.log(ship);
+
+  highlightShipArea(e.target.id, ship);
+}
+
+function dropShip(e) {
+  const startId = e.target.id;
+  const ship = ships[draggedShip.id];
+  addShip("player", ship, startId);
+  if (!notDropped) draggedShip.remove();
+}
+
+function highlightShipArea(startIndex, ship) {
+  const isHorizontal = angle === 0;
+
+  const { shipBlocks, isValid, notTaken } = checkValidity(
+    playerBoard,
+    isHorizontal,
+    startIndex,
+    ship
+  );
+
+  if (isValid && notTaken) {
+    shipBlocks.forEach((block) => {
+      block.classList.add("hover");
+      setTimeout(() => block.classList.remove("hover"), 500);
+    });
+  }
+}
+
+// GAME LOGIC
+
+let gameOver = false;
+let playerTurn;
+
+// start the game
+const startButton = document.getElementById("start-button");
+const infoDisplay = document.getElementById("info-display");
+const turnDisplay = document.getElementById("turn-display");
+
+function startGame() {
+  if (playerTurn === undefined) {
+    if (shipContainer.children.length !== 0) {
+      infoDisplay.textContent = "Please place all your ships first!";
+    } else {
+      const boardBlocks = document.querySelectorAll("#computer div");
+      boardBlocks.forEach((block) =>
+        block.addEventListener("click", handleClick)
+      );
+      playerTurn = true;
+      turnDisplay.textContent = "Your turn!";
+      infoDisplay.textContent = "The battle has begun!";
+    }
+  }
+}
+startButton.addEventListener("click", startGame);
+
+let playerHits = [];
+let computerHits = [];
+const playerSunkShips = [];
+const computerSunkShips = [];
+
+function handleClick(e) {
+  if (!gameOver) {
+    if (e.target.classList.contains("filled")) {
+      e.target.classList.add("hit");
+      infoDisplay.textContent = "You hit the enemy ship!";
+      const classes = Array.from(e.target.classList).filter(
+        (name) => !["block", "hit", "filled"].includes(name)
+      );
+      playerHits.push(...classes);
+      checkScore("player", playerHits, playerSunkShips);
+    }
+    if (!e.target.classList.contains("filled")) {
+      infoDisplay.textContent = "Miss!";
+      e.target.classList.add("miss");
+    }
+    playerTurn = false;
+    const boardBlocks = document.querySelectorAll("#computer div");
+    boardBlocks.forEach(
+      (block) => block.replaceWith(block.cloneNode(true)) // to remove event listeners
+    );
+    setTimeout(computersTurn, 500);
+  }
+}
+
+// computers turn
+function computersTurn() {
+  if (!gameOver) {
+    turnDisplay.textContent = "Computers Turn";
+    infoDisplay.textContent = "Making calculations..";
+
+    setTimeout(() => {
+      const randomShot = Math.floor(Math.random() * 10 * 10);
+      if (
+        playerBoard[randomShot].classList.contains("filled") &&
+        playerBoard[randomShot].classList.contains("hit")
+      ) {
+        computersTurn();
+      } else if (
+        playerBoard[randomShot].classList.contains("filled") &&
+        !playerBoard[randomShot].classList.contains("hit")
+      ) {
+        playerBoard[randomShot].classList.add("hit");
+        infoDisplay.textContent = "Hit the target!";
+        const classes = Array.from(playerBoard[randomShot].classList).filter(
+          (name) => !["block", "hit", "filled"].includes(name)
+        );
+        computerHits.push(...classes);
+        checkScore("computer", computerHits, computerSunkShips);
+      } else {
+        infoDisplay.textContent = "Miss!";
+        playerBoard[randomShot].classList.add("miss");
+      }
+    }, 500);
+
+    setTimeout(() => {
+      playerTurn = true;
+      turnDisplay.textContent = "Player's turn";
+      infoDisplay.textContent = "Take your shot!";
+
+      const boardBlocks = document.querySelectorAll("#computer div");
+      boardBlocks.forEach((block) =>
+        block.addEventListener("click", handleClick)
+      ); // re-adding event listeners
+    }, 500);
+  }
+}
+
+function checkScore(user, userHits, userSunkShips) {
+  function checkShip(shipName, shipLength) {
+    if (
+      userHits.filter((hitShip) => hitShip === shipName).length === shipLength
+    ) {
+      if (user === "player") {
+        infoDisplay.textContent = `You sunk the enemy ${shipName}!`;
+
+        playerHits = userHits.filter((hitShip) => hitShip !== shipName);
+      }
+      if (user === "computer") {
+        infoDisplay.textContent = `The enemy sunk your ${shipName}!`;
+        computerHits = userHits.filter((hitShip) => hitShip !== shipName);
+      }
+      userSunkShips.push(shipName);
+    }
+  }
+  checkShip("destroyer", 2);
+  checkShip("submarine", 3);
+  checkShip("cruiser", 3);
+  checkShip("battleship", 4);
+  checkShip("carrier", 5);
+  console.log("playerHits", playerHits);
+  console.log("playerSunkShips", playerSunkShips);
+
+  if (playerSunkShips.length === 5) {
+    infoDisplay.textContent =
+      "You sunk all the enemy ships! Well fought admiral!";
+    gameOver = true;
+  }
+  if (computerSunkShips.length === 5) {
+    infoDisplay.textContent =
+      "Your fleet has been destroyed! Well fought admiral, we'll get them next time.";
+    gameOver = true;
+  }
 }
