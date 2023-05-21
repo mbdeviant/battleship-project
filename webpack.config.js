2dc21c226a96865e11320383dc2f6e43aecbff9d
deviant <mb.deviant@gmail.com>
2023-05-21 20:44:24 +0300

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require("path");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    devtool: "inline-source-map",
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  externals: [nodeExternals()],
+  devtool: "inline-source-map",
 };
