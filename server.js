56b4ac4b8c83b2c9dbd3bb16456fb6538dbbbefa
deviant <mb.deviant@gmail.com>
2023-05-22 01:20:10 +0300

    create initial server connection

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+/* eslint-disable import/newline-after-import */
 /* eslint-disable import/no-extraneous-dependencies */
 const express = require("express");
-
+const path = require("path");
+const http = require("http");
+const PORT = process.env.PORT || 3000;
+const socketio = require("socket.io");
 const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Serve static files from the 'dist' directory
-app.use(express.static("dist"));
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+server.listen(PORT, () =>
+  console.log(`server is running on localhost:${PORT}`)
+);
 
 io.on("connection", (socket) => {
   console.log("A user connected");
@@ -15,6 +24,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+// server.listen(3000, () => {
+//   console.log("Server is running on http://localhost:3000");
+// });
